9205baeb3a9cf09103a7c62ad237bd714e88b37c
GreenerHorn <vivekkundariya1996@gmail.com>
2018-05-03 17:43:18 +0530

    Fix indent in utils

--- Utils.py
+++ Utils.py
@@ -3,34 +3,25 @@ import Log
 import time
 import urllib
 
-
 def is_empty_string(string):
     return string is None or string == ""
-
-
-"""Generate random number between 0.7 and 1.7"""
-
 
 def get_random_number():
     Log.log("Generating random number 1+")
     return random.random() + 1
-
 
 def random_wait():
     Log.log("random wait")
     time.sleep(get_random_number())
     return
 
-
 def remove_non_ascii(text):
     return ''.join([i if ord(i) < 128 else ' ' for i in text])
-
 
 def url_encode(query):
     encoded = urllib.parse.quote(query)
     Log.log("encoded", query, encoded)
     return encoded
-
 
 def tier_city(data):
     city_tier = 'Tier 3 City'
@@ -46,7 +37,7 @@ def tier_city(data):
               'patna', 'pondicherry', 'raipur', 'rajkot', 'rajahmundry', 'ranchi', 'rourkela', 'salem', 'sangli',
               'siliguri', 'solapur', 'srinagar', 'surat', 'thiruvananthapuram', 'palakkad', 'thrissur',
               'tiruchirappalli', 'tiruppur', 'ujjain', 'vijayapura', 'vadodara', 'varanasi', 'vasai-virar city',
-              'vijayawada','visakhapatnam','warangal']
+              'vijayawada', 'visakhapatnam', 'warangal']
     for i in city_x:
         if i.lower() in data.lower():
             city_tier = 'Tier 1 City'
