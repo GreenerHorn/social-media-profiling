4f13f23c43de58681ead66c87009d2efc971d909
Vivek Kundariya <vivekkundariya1996@gmail.com>
2018-06-17 19:58:08 +0530

    Update Constants.py
    Update

--- Constants.py
+++ Constants.py
@@ -5,11 +5,11 @@ DB_UserData_Collection = 'UserData'
 DB_Recommendation_collection = "RecommendData"
 DB_Recommender_collection = "RecommenderData"
 
-FB_ID = "ajaysingh051986@gmail.com"
-FB_PASS = "qwerty1234"
+FB_ID = ""
+FB_PASS = ""
 
-TWITTER_ID = "gj8337"
-TWITTER_PASS = "5friends"
+TWITTER_ID = ""
+TWITTER_PASS = ""
 
-LINKEDIN_ID = 'gujjujalsa1@gmail.com'
-LINKEDIN_PASS = '5friends$'
+LINKEDIN_ID = ""
+LINKEDIN_PASS = ""
